4c92688f4005062f2b63e788b05832b1aaf7e7db
Justin Ellis <jdellis490@gmail.com>
2022-06-26 17:25:30 -0700

    MVC paradigm file setup and intial code

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const session = require('express-session');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+var exphbs  = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+
+
+
+const sess = {
+    secret: 'Iron Maiden',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInterval: 15 * 60 * 1000,
+        expiration: 24 * 60 * 60 * 1000
+    })
+};
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(routes);
+
+sequelize.sync ({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port 3001!'));
+})
