ee1de5d6d14d91256944bd5e13bf34ddefeb94d0
Justin Ellis <jdellis490@gmail.com>
2022-07-11 18:23:20 -0700

    test

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 5432
 });
 
 module.exports = sequelize
